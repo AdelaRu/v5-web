4ee5689845e31f309efd1f16da2ad1209ce8ee97
adela <adela.ruckerova@gmail.com>
2026-05-07 12:13:53 +0200

    improve design

--- server.js
+++ server.js
@@ -18,4 +18,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
+});app.get("/about", (req, res) => {
+  res.sendFile(path.join(frontendPath, "about.html"));
 });
